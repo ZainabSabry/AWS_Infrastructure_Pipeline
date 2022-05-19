753b921df254878e30d46bf16a8491a29fd9ff84
Zainab Sabry <zainab.hamd@gmail.com>
2022-05-19 19:40:20 +0200

    delete unnecessary files

--- scripts/ips.sh
+++ scripts/ips.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pwd
-sed -i "s#HostName.*#HostName $(cat public_ip)#" /var/jenkins_home/.ssh/config
-sed -i "s/.*ansible_host.*/ansible_host: $(cat private_ip)/" ../ansible/group_vars/slave      
-sed -i "s#proxy_pass.*#proxy_pass http://$(cat private_ip):3000;#" ../ansible/files/nginx.conf
+sed -i "s#HostName.*#HostName $(cat ./scripts/public_ip)#" /var/jenkins_home/.ssh/config
+sed -i "s/.*ansible_host.*/ansible_host: $(cat ./scripts/private_ip)/" ../ansible/group_vars/slave      
+sed -i "s#proxy_pass.*#proxy_pass http://$(cat ./scripts/private_ip):3000;#" ../ansible/files/nginx.conf
 
 
 
