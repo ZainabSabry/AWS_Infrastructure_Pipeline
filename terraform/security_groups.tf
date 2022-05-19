c3eeda2c704dc57b650d8f1a539b00e39427776a
Zainab Sabry <zainab.hamd@gmail.com>
2022-05-19 22:23:45 +0200

    sg for alb

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -125,3 +125,30 @@ resource "aws_security_group" "rds_sg" {
     Name = "${var.name}-rds_sg"
   }
 }
+
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb sg"
+  description = "alb sg"
+  vpc_id      = module.network.vpc_id
+
+  ingress {
+    description = "80 from everywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-alb_sg"
+  }
+}
