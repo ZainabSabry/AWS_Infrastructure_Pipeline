28aca2b60f22e845145e46ed05c542152653e3c9
Zainab Sabry <zainab.hamd@gmail.com>
2022-05-19 18:43:42 +0200

    fix

--- scripts/ips.sh
+++ scripts/ips.sh
@@ -2,7 +2,7 @@
 
 sed -i "s#HostName.*#HostName $(cat public_ip)#" /var/jenkins_home/.ssh/config
 sed -i "s/.*ansible_host.*/ansible_host: $(cat private_ip)/" ../ansible/group_vars/slave      
-sed -i 's#proxy_pass.*#proxy_pass http://$(cat private_ip):3000;#' ../ansible/files/nginx.conf
+sed -i "s#proxy_pass.*#proxy_pass http://$(cat private_ip):3000;#" ../ansible/files/nginx.conf
 
 
 
