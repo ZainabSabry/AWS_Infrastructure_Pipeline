b587eb7c6b50bd0559953d675fc75e61c6e2a860
Zainab Sabry <zainab.hamd@gmail.com>
2022-05-19 20:01:17 +0200

    testing ips

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "bastion" {
   ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.public.id]
-  subnet_id              = module.network.public_2_id
+  subnet_id              = module.network.public_1_id
   # key_name               = aws_key_pair.public_key_pair.key_name
   key_name               = "redhat"
 
@@ -16,7 +16,7 @@ resource "aws_instance" "application" {
   ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id              = module.network.private_1_id
+  subnet_id              = module.network.private_2_id
   # key_name               = aws_key_pair.public_key_pair.key_name
   key_name               = "redhat"
 
