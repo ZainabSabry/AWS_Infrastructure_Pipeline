48bdb2035d4d094bf4e9f54e7a5fe770a6ec874c
Zainab Sabry <zainab.hamd@gmail.com>
2022-05-19 19:41:31 +0200

    edit

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -2,17 +2,12 @@ resource "aws_instance" "bastion" {
   ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.public.id]
-  subnet_id              = module.network.public_1_id
+  subnet_id              = module.network.public_2_id
   # key_name               = aws_key_pair.public_key_pair.key_name
   key_name               = "redhat"
 
   tags = {
     Name = "${var.name}-bastion"
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > ../scripts/public_ip"
-    
   }
 }
 
@@ -21,16 +16,12 @@ resource "aws_instance" "application" {
   ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id              = module.network.private_2_id
+  subnet_id              = module.network.private_1_id
   # key_name               = aws_key_pair.public_key_pair.key_name
   key_name               = "redhat"
 
   tags = {
     Name = "${var.name}-application"
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${self.private_ip} > ../scripts/private_ip"      
   }
 
   # provisioner "local-exec" {
