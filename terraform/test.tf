a98bbc03206b4bbe1a8d7ce06c2671dacd380eb0
Zainab Sabry <zainab.hamd@gmail.com>
2022-05-19 17:22:18 +0200

    testing sth with outputs

--- terraform/test.tf
+++ terraform/test.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test" {
   ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id              = module.network.private_2_id
+  subnet_id              = module.network.private_1_id
   # key_name               = aws_key_pair.public_key_pair.key_name
   key_name               = "redhat"
 
@@ -11,11 +11,10 @@ resource "aws_instance" "test" {
   }
 
   provisioner "local-exec" {
-    command = "sed -i 's/.*ansible_host.*/ansible_host: ${self.private_ip}/' ../ansible/group_vars/slave"
-  }
-
-   provisioner "local-exec" {
-    command = "sed -i 's#proxy_pass.*#proxy_pass http://${self.private_ip}:3000;#' ../ansible/files/nginx.conf" 
+    command = <<-EOT
+    sed -i 's/.*ansible_host.*/ansible_host: ${self.private_ip}/' ../ansible/group_vars/slave      ./sed -i 's#proxy_pass.*#proxy_pass http://${self.private_ip}:3000;#' ../ansible/files/nginx.conf
+    EOT
+    
   }
 
 }
