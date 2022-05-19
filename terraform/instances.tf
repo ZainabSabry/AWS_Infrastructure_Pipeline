4d83e81d7b067f4e054b513523e3c445f0830d7a
Zainab Sabry <zainab.hamd@gmail.com>
2022-05-19 17:35:32 +0200

    testing sth with outputs

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "application" {
   ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id              = module.network.private_1_id
+  subnet_id              = module.network.private_2_id
   # key_name               = aws_key_pair.public_key_pair.key_name
   key_name               = "redhat"
 
@@ -30,9 +30,9 @@ resource "aws_instance" "application" {
   }
 
   provisioner "local-exec" {
-    command = <<EOT
-         sed -i 's/.*ansible_host.*/ansible_host: ${self.private_ip}/' ../ansible/group_vars/slave
-        sed -i 's#proxy_pass.*#proxy_pass http://${self.private_ip}:3000;#' ../ansible/files/nginx.conf
+    command = <<-EOT
+    sed -i 's/.*ansible_host.*/ansible_host: ${self.private_ip}/' ../ansible/group_vars/slave      
+    sed -i 's#proxy_pass.*#proxy_pass http://${self.private_ip}:3000;#' ../ansible/files/nginx.conf
     EOT
     
     
