4f7af7efec3e31ee2c4972c26e215e0be58486c6
Zainab Sabry <zainab.hamd@gmail.com>
2022-05-19 18:28:16 +0200

    script to copy newly-created-instance ips

---
+++ scripts/ips.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sed -i "s#HostName.*#HostName $(cat public_ip)#" /var/jenkins_home/.ssh/config
+sed -i "s/.*ansible_host.*/ansible_host: $(cat private_ip)/" ../ansible/group_vars/slave      
+sed -i 's#proxy_pass.*#proxy_pass http://$(cat private_ip):3000;#' ../ansible/files/nginx.conf
+
+
+
+
+
+
+
+
+
+
+# sed -i 's#HostName.*#HostName ${self.public_ip}#' /var/jenkins_home/.ssh/config
+# sed -i 's/.*ansible_host.*/ansible_host: ${self.private_ip}/' ../ansible/group_vars/slave      
+# sed -i 's#proxy_pass.*#proxy_pass http://${self.private_ip}:3000;#' ../ansible/files/nginx.conf
