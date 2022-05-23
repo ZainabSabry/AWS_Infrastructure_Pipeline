ebe944147c0c9d91f61559205f6464ec26edcbd9
Zainab Sabry <zainab.hamd@gmail.com>
2022-05-23 23:14:57 +0200

    using data_resources for username and password

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -13,9 +13,19 @@ resource "aws_db_instance" "terra-rds" {
   engine                 = "mysql"
   engine_version         = "8.0.28"
   instance_class         = "db.t2.micro"
-  username               = "admin"
-  password               = "password123456789"
+  username               = data.aws_ssm_parameter.rds_username.value
+  password               = data.aws_ssm_parameter.rds_password.value
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.terra-subnet-group.name
+}
+
+
+data "aws_ssm_parameter" "rds_username" {
+  name = "/dev/database/username"
+}
+
+
+data "aws_ssm_parameter" "rds_password" {
+  name = "/dev/database/password"
 }
